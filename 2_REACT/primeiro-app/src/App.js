252e8a3b18de13e29523ec9cdb8de95ca1e162f6
Eduardo Olimpio <106188291+eduardoolimpiodev@users.noreply.github.com>
2023-04-25 22:23:33 -0300

    feature: props e componente

--- 2_REACT/primeiro-app/src/App.js
+++ 2_REACT/primeiro-app/src/App.js
@@ -1,10 +1,15 @@
+import Nome from "./components/Nome";
+
 function App() {
   return (
-    <div >
-      <h1>Bem vindo ao meu projeto</h1>
-
-    </div>
-  );
+    <>
+      <h2 className="container">Componente App</h2>
+      <Nome aluno="Eduardo" idade={37} />
+      <br />
+      <Nome aluno="Eduardo Olimpio" idade={38}/>
+    </>
+  )
 }
 
 export default App;
+
