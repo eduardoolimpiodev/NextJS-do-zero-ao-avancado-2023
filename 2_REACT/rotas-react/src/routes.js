b86e39a28220a52056f027696c048faea0f788ec
Eduardo Olimpio <106188291+eduardoolimpiodev@users.noreply.github.com>
2023-04-27 16:59:35 -0300

    feature: Trabalhando com rotas 2

--- 2_REACT/rotas-react/src/routes.js
+++ 2_REACT/rotas-react/src/routes.js
@@ -3,6 +3,7 @@ import Home from './pages/Home'
 import Sobre from './pages/Sobre';
 import Contato from './pages/Contato';
 import Erro from './pages/Erro';
+import Produto from './pages/Produto';
 import Header from './components/Header';
 
 const Routes = () => {
@@ -13,6 +14,8 @@ const Routes = () => {
                 <Route exact path="/" component={Home} />
                 <Route path="/sobre" component={Sobre} />
                 <Route exact path="/contato" component={Contato} />
+                <Route path="/produto/:id" component={Produto} />
+                
                 <Route path="*" component={Erro} />
             </switch>
         </BrowserRouter>
