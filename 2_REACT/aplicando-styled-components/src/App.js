1e6f0f276a053dbc5b5538a2057c541f81033f97
Eduardo Olimpio <106188291+eduardoolimpiodev@users.noreply.github.com>
2023-04-27 19:27:34 -0300

    add: Styled Components

---
+++ 2_REACT/aplicando-styled-components/src/App.js
@@ -0,0 +1,25 @@
+
+import { Container, Head, Titulo} from "./styles"; 
+
+function App() {
+  return (
+    <Container>
+      <Head>
+        <Titulo>Projeto Styled</Titulo>
+      </Head>
+
+    </Container>
+  );
+}
+
+export default App;
+
+
+/* 
+ <div className="container">
+      <header className='header'>
+        <p className='titulo'>Projeto Styled</p>
+      </header>
+        <h1>Bem vindo ao sistema!</h1>
+    </div>
+*/
---
+++ 2_REACT/aplicando-styled-components/src/styles.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components'
+
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 500px;
+`;
+
+export const Head = styled.header`
+  width: 100%;
+  height: 70px;
+  background-color: brown;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+
+  p{
+    font-size: 45px;
+    color: #FFF;
+  }
+`;
+
+export const Titulo = styled.a`
+  font-size: 25px;
+  color: #00FF00;
+`;
+
+/* 
+
+.titulo {
+  font-size: 35px;
+  color: #fff;
+}
+*/
