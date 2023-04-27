30e2a7547d0b29a99a5a962bbcd39dad8d5c4aaa
Eduardo Olimpio <106188291+eduardoolimpiodev@users.noreply.github.com>
2023-04-27 14:42:33 -0300

    feature: add Link

--- 2_REACT/rotas-react/src/pages/Contato/index.js
+++ 2_REACT/rotas-react/src/pages/Contato/index.js
@@ -1,10 +1,12 @@
-
+import { Link } from "react-router-dom";
 
 export default function Contato() {
     return (
       <div>
        <h1>Meu conato</h1> <br/>
        <span>Email: email@email.com</span>
+       <Link to="/">Home</Link> <br/>
+       <Link to="/sobre">Sobre</Link>
       </div>
     );
   }
--- 2_REACT/rotas-react/src/pages/Sobre/index.js
+++ 2_REACT/rotas-react/src/pages/Sobre/index.js
@@ -1,9 +1,11 @@
-
+import { Link } from "react-router-dom";
 
 export default function Sobre() {
     return (
       <div>
        <h1>Sobre a empresa</h1>
+       <Link to="/">Home</Link> <br/>
+       <Link to="/contato">Contato</Link>
       </div>
     );
   }
