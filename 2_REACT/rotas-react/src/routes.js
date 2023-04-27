45eab4b8ef28a314bbea126ca2269741d2fd8c3a
Eduardo Olimpio <106188291+eduardoolimpiodev@users.noreply.github.com>
2023-04-27 16:42:44 -0300

    feature: Header part 1

--- 2_REACT/rotas-react/src/routes.js
+++ 2_REACT/rotas-react/src/routes.js
@@ -2,10 +2,12 @@ import { Route, BrowserRouter, Switch} from 'react-router-dom';
 import Home from './pages/Home'
 import Sobre from './pages/Sobre';
 import Contato from './pages/Contato';
+import Header from './components/Header';
 
 const Routes = () => {
     return (
         <BrowserRouter>
+            <Header />
             <switch>
                 <Route exact path="/" component={Home} />
                 <Route path="/sobre" component={Sobre}/>
